06c596d0cbf2f81c4acca4094e7bae6395a5180d
Laurent Barbe <laurent@ksperis.com>
2026-01-18 00:50:34 +0100

    remove nginx.compose.conf

--- frontend/docker-entrypoint.d/20-use-compose-conf.sh
+++ frontend/docker-entrypoint.d/20-use-compose-conf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 
-if [ "${NGINX_CONF:-}" = "compose" ] && [ -f /etc/nginx/conf.d/nginx.compose.conf ]; then
-  cp /etc/nginx/conf.d/nginx.compose.conf /etc/nginx/conf.d/default.conf
+if [ "${NGINX_CONF:-}" = "compose" ] && [ -f /etc/nginx/compose/nginx.compose.conf ]; then
+  cp /etc/nginx/compose/nginx.compose.conf /etc/nginx/conf.d/default.conf
 fi
