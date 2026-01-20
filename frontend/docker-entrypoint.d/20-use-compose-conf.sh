76eb7451cd80745ec9b2b179df82c4fae8c8b43c
Laurent Barbe <laurent@ksperis.com>
2026-01-20 09:18:44 +0100

    remove unsed files

--- frontend/docker-entrypoint.d/20-use-compose-conf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-
-if [ "${NGINX_CONF:-}" = "compose" ] && [ -f /etc/nginx/compose/nginx.compose.conf ]; then
-  cp /etc/nginx/compose/nginx.compose.conf /etc/nginx/conf.d/default.conf
-fi
